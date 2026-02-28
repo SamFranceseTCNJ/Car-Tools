da005c71c9d8ec2de5b23d9c92438abb04d3b6ea
sonitambashta <sunnyambashta@gmail.com>
2026-02-28 17:52:29 -0500

    Dataset fully consistent

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,4 +1,8 @@
 const App = () => {
-    return <h1>Hello</h1>
+    return (
+    <>
+        <h1>Hello</h1>
+    </>
+    )
 }
 export default App
---
+++ frontend/src/components/Navbar.jsx
@@ -0,0 +1,9 @@
+const Navbar = () => {
+    return (
+        <nav>
+            <a>Home</a>
+            <a>Dashboard</a>
+        </nav>
+    )
+}
+export default Navbar
